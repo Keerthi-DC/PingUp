ea8257428653d918ee840b781af4950436ceda7d
Keerthi D C <146311325+Keerthi-DC@users.noreply.github.com>
2025-08-05 21:19:42 +0530

    Update main.jsx

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,3 +1,6 @@
+import axios from 'axios'; 
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL;
+axios.defaults.withCredentials = true;
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
